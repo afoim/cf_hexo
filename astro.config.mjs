19ee6789a3e11568b09fe12dcd65c6b7f3116784
afoim <acofork@qq.com>
2024-10-15 02:35:35 +0800

    更新文章底部的站点url

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://onani.cn",
   base: "/",
   trailingSlash: "always",
   integrations: [
